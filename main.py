fa0178a904ba228e91ed0db7c0914278c62e9f5a
Stone-keep <radek12345@gmail.com>
2026-03-29 21:05:06 +0200

    added 60 FPS cap

--- main.py
+++ main.py
@@ -4,9 +4,15 @@ from logger import log_state
 
 def main():
     
+    print("Starting Asteroids with pygame version: 2.6.1")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    
+    clock = pygame.time.Clock()
+    dt = 0
+
     while True:
         log_state()
 
@@ -17,9 +23,10 @@ def main():
         screen.fill("black")
         pygame.display.flip()
 
-        print("Starting Asteroids with pygame version: 2.6.1")
-        print(f"Screen width: {SCREEN_WIDTH}")
-        print(f"Screen height: {SCREEN_HEIGHT}")
+        clock.tick(60)
+        dt = clock.tick(60) / 1000
+
+        
 
 
 if __name__ == "__main__":
